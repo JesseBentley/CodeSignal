a2e78f895c7feb6c3433f1247f05bee56e639c9e
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 05:46:13 -0400

    Added challenge description to centuryFromYear.py

--- Arcade/The Journey Begins/centuryFromYear.py
+++ Arcade/The Journey Begins/centuryFromYear.py
@@ -1,3 +1,20 @@
+#Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+#Example
+#For year = 1905, the output should be
+#centuryFromYear(year) = 20;
+#For year = 1700, the output should be
+#centuryFromYear(year) = 17.
+#Input/Output
+#[execution time limit] 4 seconds (py3)
+#[input] integer year
+#A positive integer, designating the year.
+#Guaranteed constraints:
+#1 ≤ year ≤ 2005.
+#[output] integer
+#The number of the century the year is in.
+
+#Solution
+
 def centuryFromYear(year):
     return 1 + (year - 1) // 100
 
