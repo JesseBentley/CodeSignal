c0dddc7ba4c15805bd8b0d2f185d46168e5169c3
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 05:44:59 -0400

    Added challenge description to add.py

--- Arcade/The Journey Begins/add.py
+++ Arcade/The Journey Begins/add.py
@@ -1,2 +1,19 @@
+#Write a function that returns the sum of two numbers.
+#Example
+#For param1 = 1 and param2 = 2, the output should be
+#add(param1, param2) = 3.
+#Input/Output
+#[execution time limit] 4 seconds (py3)
+#[input] integer param1
+#Guaranteed constraints:
+#-1000 ≤ param1 ≤ 1000.
+#[input] integer param2
+#Guaranteed constraints:
+#-1000 ≤ param2 ≤ 1000.
+#[output] integer
+#The sum of the two inputs.
+
+#Solution
+
 def add(param1, param2):
     return param1 + param2
