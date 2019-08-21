9ce498c657c4642bafc8aa9b5e8b8b0bef2fae5a
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-21 05:42:34 -0400

    matrixElementSum solution added in python

---
+++ Arcade/Edge of The Ocean/matrixElementSum.py
@@ -0,0 +1,18 @@
+# After becoming famous, the CodeBots decided to move into a new building together. Each of the rooms has a different cost, 
+# and some of them are free, but there's 
+# a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, 
+# they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+
+# Solution
+
+def matrixElementsSum(matrix):
+    total = 0;
+    for j in range(0, len(matrix[0])):
+        for i in range(0, len(matrix)):
+            if( matrix[i][j] == 0):
+                break
+            else:
+                total += matrix[i][j]
+    else:
+        return total
+
